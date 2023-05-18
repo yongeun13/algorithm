9c81e10ea71ed0a2eac23b6b5bc9bd42203138c5
yongeun13 <asp1135@gmail.com>
2023-05-18 23:18:10 +0900

    0518

---
+++ 백준/G1-비트+dp-외판원순회.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int N;
+int cost[16][16];
+int dp[1 << 15][16];
+int fullV;
+
+int dfs(int now, int V) {
+
+	if (V == fullV) {//모두 방문한 경우
+		if (cost[now][0] != 0) return cost[now][0];//시작노드와 이어지면
+		else return 21e8;//아니면 -> 불가능
+	}
+	if (dp[V][now] != 0) return dp[V][now];
+
+	int temp = 21e8;
+
+	for (int i = 1; i < N; i++) { //시작노드는 방문 X
+		int next = i;
+		if (cost[now][next] == 0) continue;//연결 안된경우 pass
+		if (((V >> (next - 1)) & 1) == 1) continue;//방문한 경로 pass
+		int newV = V | (1 << (next - 1));
+
+		temp = min(temp, dfs(next,newV) + cost[now][next]);
+	}
+
+	return dp[V][now] = temp;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			cin >> cost[y][x];
+		}
+	}
+	fullV = pow(2, N-1)-1;
+	
+	cout << dfs(0,0);
+	return 0;
+}
---
+++ 백준/S1-분할정복+재귀-Z.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int Count = 0;
+int N, r, c;
+
+//(row, col)은 해당 사분면의 왼쪽위 좌표
+void search(int row, int col, int size)
+{
+    if (r == row && c == col)
+    {
+        cout << Count;
+        return;
+    }
+    if (r < row + size && r >= row && c < col + size && c >= col) // (r, c)가 해당 사분면에 존재하면
+    {
+        search(row, col, size / 2);
+        search(row, col + size / 2, size / 2);
+        search(row + size / 2, col, size / 2);
+        search(row + size / 2, col + size / 2, size / 2);
+    }
+    else // (r, c)가 범위 내에 없으면
+    {
+        Count += (size * size);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> r >> c;
+
+    search(0, 0, pow(2, N));
+}
